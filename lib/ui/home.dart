2d3ab77ffedabebd4f57abcc85722ae645aa5a67
Ogola Sospeter <okongo.ogola1@students.jkuat.ac.ke>
2024-08-30 18:32:45 +0300

    Home Page

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -10,23 +10,356 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.primary,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Welcome 🎉",
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-            const SizedBox(height: 10),
-            Text(
-              _boxLogin.get("userName"),
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-          ],
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.menu, color: Colors.blue),
+          onPressed: () {},
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(0),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(13)),
+              child: Container(
+                decoration: const BoxDecoration(
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: Color(0xfff8f8f8),
+                      blurRadius: 10,
+                      spreadRadius: 10,
+                    ),
+                  ],
+                ),
+                child: Image.asset(
+                  'assets/profile.jpg',
+                  width: 40,
+                  height: 40,
+                ),
+              ),
+            ),
+          ),
+          PopupMenuButton(
+            itemBuilder: (BuildContext context) {
+              return [
+                const PopupMenuItem(
+                  child: Text("Profile"),
+                ),
+                PopupMenuItem(
+                  child: ListTile(
+                    title: const Text("Logout"),
+                    onTap: () {
+                      _boxLogin.put("loginStatus", false);
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Login(),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ];
+            },
+          ),
+        ],
+      ),
+      body: const MainBody(),
+    );
+  }
+}
+
+class MainBody extends StatelessWidget {
+  const MainBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 20),
+          const Text(
+            "Our",
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const Text(
+            "Products",
+            style: TextStyle(
+              fontSize: 43,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      hintText: "Search for products",
+                      border: InputBorder.none,
+                      icon: Icon(Icons.search),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: IconButton(
+                  icon: const Icon(Icons.filter_alt_outlined),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          //add a list of clothes/ wearables with each item in a card and with a leading icon
+          const Text(
+            "Categories",
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20),
+          //the categories include Shirts, Trousers, Shoes, Hats, Sneakers, Suits, Dresses, Skirts, and Accessories
+          //use a scrollable row to display the categories
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            reverse: true,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                categoryWidget("Shirts", Icons.shopping_bag),
+                const SizedBox(width: 20), // Spacing between boxes
+                categoryWidget("Trousers", Icons.fastfood),
+                const SizedBox(width: 20), // Spacing between boxes
+                categoryWidget("Shoes", Icons.local_drink),
+                const SizedBox(width: 20), // Spacing between boxes
+                categoryWidget("Hats", Icons.shopping_bag),
+                const SizedBox(width: 20), // Spacing between boxes
+                categoryWidget("Sneakers", Icons.fastfood),
+                const SizedBox(width: 20), // Spacing between boxes
+                categoryWidget("Suits", Icons.local_drink),
+                const SizedBox(width: 20), // Spacing between boxes
+                categoryWidget("Dresses", Icons.shopping_bag),
+                const SizedBox(width: 20), // Spacing between boxes
+                categoryWidget("Skirts", Icons.fastfood),
+                const SizedBox(width: 20), // Spacing between boxes
+                categoryWidget("Accessories", Icons.local_drink),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+//create a reusable widget for the categories
+Widget categoryWidget(String category, IconData icon) {
+  return Column(
+    children: [
+      Container(
+        width: 100,
+        height: 50,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: Colors.blue,
+            width: 1,
+          ),
+          shape: BoxShape.rectangle,
+        ),
+        child: Icon(
+          icon,
+          size: 25,
+          color: Colors.blue,
+        ),
+      ),
+      const SizedBox(height: 10),
+      Text(category, textAlign: TextAlign.center),
+    ],
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+     const Text(
+          "Categories",
+          textAlign: TextAlign.left,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Icon(
+                    Icons.shopping_bag,
+                    size: 50,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text("Grocery"),
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Icon(
+                    Icons.fastfood,
+                    size: 50,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text("Food"),
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Icon(
+                    Icons.local_drink,
+                    size: 50,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text("Drinks"),
+              ],
+            ),
+          ],
+        ),
+        const SizedBox(height: 20),
+        const Text(
+          "Popular",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Icon(
+                    Icons.shopping_bag,
+                    size: 50,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text("Grocery"),
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Icon(
+                    Icons.fastfood,
+                    size: 50,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text("Food"),
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Icon(
+                    Icons.local_drink,
+                    size: 50,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text("Drinks"),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+*/
